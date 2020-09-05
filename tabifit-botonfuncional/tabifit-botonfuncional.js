c99697f71f6178b4624a04df1ea0b5f9c3beaee0
AnaCanalis <anita_canalis@hotmail.com>
2020-09-05 14:17:10 -0300

    css en web components

--- tabifit-botonfuncional/tabifit-botonfuncional.js
+++ tabifit-botonfuncional/tabifit-botonfuncional.js
@@ -2,12 +2,10 @@ class BotonFuncional extends HTMLElement {
     constructor(){
       super();
       cargarTemplate("#tabifit-botonfuncional", this);    
-      this.tipoBoton = this.getAttribute("tipo")
      }
      
      connectedCallback() {
       const elementoBoton = this.shadowRoot.querySelector("button");
-      elementoBoton.className = this.tipoBoton;
     }    
   }
 
